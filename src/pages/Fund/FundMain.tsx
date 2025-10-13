bc15ba6cb2fdcb9768ba8dafe6cb4fb53635fe94
kaviyakumar23 <kaviyakumar23@gmail.com>
2025-10-13 23:18:37 +0530

    Update Fund Main

--- src/pages/Fund/FundMain.tsx
+++ src/pages/Fund/FundMain.tsx
@@ -6,6 +6,8 @@ import FundPicks from "./FundPicks.tsx"
 import ChooseCard from "./ChooseCard.tsx"
 import InvestmentReturns from "./InvestmentReturns.tsx"
 import Ratios from "./Ratios.tsx"
+import Carrva from "./Carrva.tsx"
+import AssetAllocation from "./AssetAllocation.tsx"
 import FundTabs from "./FundTabs.tsx"
 
 const FundMain = () => {
@@ -23,17 +25,19 @@ const FundMain = () => {
         <BasicInfo />
 
         {/* Desktop View - All components stacked (lg and above) */}
-        <div className="hidden xl:block px-4 py-2 md:px-6 md:py-4 lg:px-20 lg:py-10">
+        <div className="hidden xl:block px-4 py-2 md:block lg:px-20 lg:py-10">
           <FundPicks onCategorySelect={setSelectedCategory} />
           <div className="">
             <ChooseCard selectedCategory={selectedCategory} />
             <InvestmentReturns />
             <Ratios />
+            <Carrva />
+            <AssetAllocation />
           </div>
         </div>
 
         {/* Mobile/Tablet View - Tabbed interface (below lg) */}
-        <div className="xl:hidden">
+        <div className="md:hidden xl:hidden">
           <FundTabs
             selectedCategory={selectedCategory}
             onCategorySelect={setSelectedCategory}
